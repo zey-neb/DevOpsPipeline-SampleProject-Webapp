008a5e8049ebdb333d32fe333f8bf54877e24c34
zeyneb sdiri <zeyneb.sdiri@gmail.com>
2020-07-01 19:58:10 +0100

    changed

--- Katalon.sh
+++ Katalon.sh
@@ -1,131 +1,70 @@
 echo "Katalon workspace folder is set to default location: /tmp/session-cb7ae4c4
-
 Starting Groovy-Eclipse compiler resolver.  Specified compiler level: unspecified
-
 176 2.4.7.xx-201611170128-e46 = ACTIVE
 
 
-
 INFO: Katalon Version: 7.4.0
-
 INFO: Command-line arguments: -runMode=console -projectPath=/home/zeyneb/Downloads/webapp/webapp/webapp.prj -retry=3 -retryFailedTestCases=true -testSuitePath=Test Suites/testsuite1 -executionProfile=default -browserType=Firefox -apiKey=******
-
 INFO: User working dir: /home/zeyneb/Katalon_Studio_Engine_Linux_64-7.4.0
-
 INFO: Error log: /tmp/session-cb7ae4c4/.metadata/.log
-
 INFO: Katalon TestOps server URL: https://analytics.katalon.com
-
 INFO: Katalon Store server URL: https://store.katalon.com
-
 INFO: User home: /home/zeyneb
-
 INFO: Java vendor: Private Build
-
 INFO: Java version: 1.8.0_252
-
 INFO: Local OS: Linux 64bit
-
 INFO: CPU load: 0%
-
 INFO: Total memory: 7988 MB
-
 INFO: Free memory: 147 MB
-
 INFO: Machine ID: d0a370e1c0c30dd604c87ed26005377a
 
 
-
 Activating...
-
 Start activating offline...
-
 Search for valid offline licenses in folder: /home/zeyneb/.katalon/license
-
 The number of valid offline licenses: 0
-
 Offline activation failed.
-
 Start activating online...
-
 Delete folder: bin
-
 Delete folder: Libs
-
 Cleaning up workspace
-
 Opening project file: /home/zeyneb/Downloads/webapp/webapp/webapp.prj
-
 Generating global variables...
-
 Parsing custom keywords...
-
 Project 'webapp' opened
-
 Start reloading plugins...
-
 Katalon version: 7.4.0
-
 Plugin info URL: https://store.katalon.com/api/products/ks?appVersion=7.4.0&appType=ENGINE&licenseType=TRIAL
-
 Start check license task
-
 gecko driver is located at default location: /home/zeyneb/Katalon_Studio_Engine_Linux_64-7.4.0/configuration/resources/drivers/firefox_linux64/geckodriver. In case your browser is updated to a newer version, please use this command to update gecko driver: --config -webui.autoUpdateDrivers=true
 
-
 --------------------------------------------------------------------------------
-
 Test Suites/testsuite1 - Firefox - 20200529_212046.......................0/0(0%)
-
 --------------------------------------------------------------------------------
-
 
 2020-05-29 21:20:59.061 INFO  c.k.katalon.core.main.TestSuiteExecutor  - START Test Suites/testsuite1
-
 2020-05-29 21:20:59.280 INFO  c.k.katalon.core.main.TestSuiteExecutor  - hostName = zeyneb - box
-
 2020-05-29 21:20:59.284 INFO  c.k.katalon.core.main.TestSuiteExecutor  - os = Linux 64bit
-
 2020-05-29 21:20:59.285 INFO  c.k.katalon.core.main.TestSuiteExecutor  - hostAddress = 192.168.119.141
-
 2020-05-29 21:20:59.291 INFO  c.k.katalon.core.main.TestSuiteExecutor  - katalonVersion = 7.4.0.1
-
 2020-05-29 21:21:01.048 INFO  c.k.katalon.core.main.TestSuiteExecutor  - --------------------
-
 2020-05-29 21:21:01.057 INFO  c.k.katalon.core.main.TestSuiteExecutor  - END Test Suites/testsuite1
-
 2020-05-29 21:21:01.060 INFO  c.k.katalon.core.main.TestSuiteExecutor  - ====================
-
 Launcher status after execution process completed: Running
-
 Start writing execution.uuid file to folder: /home/zeyneb/Downloads/webapp/webapp/Reports/20200529_212046/testsuite1/20200529_212046
-
 Start generating JUnit report folder at: /home/zeyneb/Downloads/webapp/webapp/Reports/20200529_212046/testsuite1/20200529_212046...
-
 JUnit report were generated at folder: /home/zeyneb/Downloads/webapp/webapp/Reports/20200529_212046/testsuite1/20200529_212046
-
 For real-time monitoring and better reporting capabilities please integrate this project with Katalon TestOps (more details at https://docs.katalon.com/katalon-analytics/docs/integration-with-katalon-studio.html#enable-integration)
 
-
 --------------------------------------------------------------------------------
-
 Test Suites/testsuite1 - Firefox - 20200529_212046.......................0/0(0%)
-
 --------------------------------------------------------------------------------
-
 
 Execution completed. Exit code: 0.
-
 Start clean up session
-
 End check license task
-
 End clean up session
-
 Start release license task
-
 License released
-
 End release license task
-
 All launchers terminated"
